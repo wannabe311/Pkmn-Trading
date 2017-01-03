5ec9f0074b34cecebd3c7214d94f4a5a0a892028
wannabe311 <uracistmeow@gmail.com>
2017-01-03 15:10:02 +0800

    Add files via upload

--- static/config.js
+++ static/config.js
@@ -1,8 +1,8 @@
-// ***********************************************************************************
+﻿// ***********************************************************************************
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "1Fu6Ja5olNaIdEEoN0vYJnZ0e933Zb1lUM6MLI4Isur0";
 
 // ***********************************************************************************
 // Position of Breedables, Shinies and Wants worksheet
@@ -15,16 +15,16 @@ var wantsWorksheet = 3;
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "2165-8630-9919";
+var inGameName = "Armando";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/wannabe311";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "http://data.whicdn.com/images/60098948/large.png";
